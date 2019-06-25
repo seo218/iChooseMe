986aa0472ffdac59be75b99d420fa895e1a3e1d0
Hack Reactor Student <student@hackreactor.com>
2019-06-25 15:28:10 -0400

    random assignment working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import Page1 from './components/page1.js'
-import Page2 from './components/page2.js'
-import Page3 from './components/page3.js'
+import Page1 from "./components/page1.js";
+import Page2 from "./components/page2.js";
+import Page3 from "./components/page3.js";
 // import {Pagination} from 'react-bootstrap'
 
 class App extends React.Component {
@@ -16,34 +16,28 @@ class App extends React.Component {
       showPage2: false,
       page2: null,
       showPage3: false,
-      page3: null
+      page3: null,
+      showPokemon: false,
+      yourPokemon: null
     };
     this.getPokeData = this.getPokeData.bind(this);
-    this.handlePage1Click = this.handlePage1Click.bind(this)
-    this.handlePage2Click = this.handlePage2Click.bind(this)
-    this.handlePage3Click = this.handlePage3Click.bind(this)
+    this.handlePage1Click = this.handlePage1Click.bind(this);
+    this.handlePage2Click = this.handlePage2Click.bind(this);
+    this.handlePage3Click = this.handlePage3Click.bind(this);
+    this.pokemonSelector = this.pokemonSelector.bind(this);
   }
 
   componentDidMount() {
     this.getPokeData();
     this.setState({
-      page1: 
-      <Page1 
-        formComplete={this.handlePage1Click}
-      />
-    })
+      page1: <Page1 formComplete={this.handlePage1Click} />
+    });
     this.setState({
-      page2:
-      <Page2
-        formComplete={this.handlePage2Click}
-      />
-    })
+      page2: <Page2 formComplete={this.handlePage2Click} />
+    });
     this.setState({
-      page3:
-      <Page3
-        formComplete={this.handlePage3Click}
-      />
-    })
+      page3: <Page3 formComplete={this.handlePage3Click} />
+    });
   }
 
   getPokeData() {
@@ -52,11 +46,11 @@ class App extends React.Component {
       axios
         .get(`https://pokeapi.co/api/v2/pokemon/${i}`)
         .then(({ data }) => {
-          pokemon[data.name] = data;
+          pokemon[data.id] = data;
           // console.log('logging data', data.name)
           this.setState({
             pokemon: pokemon
-          })
+          });
         })
         .catch(err => {
           console.log("err in getting poke data client side", err);
@@ -64,45 +58,52 @@ class App extends React.Component {
     }
   }
 
-  handlePage1Click (e) {
-    e.preventDefault()
+  handlePage1Click(e) {
+    e.preventDefault();
     this.setState({
       showPage1: !this.state.showPage1
-    })
+    });
     this.setState({
       showPage2: !this.state.showPage2
-    })
+    });
   }
 
-  handlePage2Click (e) {
-    e.preventDefault()
+  handlePage2Click(e) {
+    e.preventDefault();
     this.setState({
       showPage2: !this.state.showPage2
-    })
+    });
     this.setState({
       showPage3: !this.state.showPage3
-    })
+    });
   }
 
-
-  handlePage3Click (e) {
-    e.preventDefault()
+  handlePage3Click(e) {
+    e.preventDefault();
     this.setState({
       showPage3: !this.state.showPage3
-    })
+    });
+    this.setState({
+      showPokemon: !this.state.showPokemon
+    });
+    this.pokemonSelector();
+  }
+
+  pokemonSelector() {
+    this.setState({
+      yourPokemon: this.state.pokemon[Math.ceil(Math.random() * 151)]
+    });
   }
 
   render() {
     return (
       <div className="iChooseMe">
-      {this.state.showPage1 ? this.state.page1 : null}
-      {this.state.showPage2 ? this.state.page2 : null}
-      {this.state.showPage3 ? this.state.page3 : null}
-      
-      
-
+        {this.state.showPage1 ? this.state.page1 : null}
+        {this.state.showPage2 ? this.state.page2 : null}
+        {this.state.showPage3 ? this.state.page3 : null}
+        {this.state.showPokemon ? this.state.yourPokemon.id : null}
       </div>
-  );
+    );
   }
 }
 
