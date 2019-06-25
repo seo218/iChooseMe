15bdd2c7ba5223aa854c8830b0703855ffc6345c
Hack Reactor Student <student@hackreactor.com>
2019-06-25 17:29:31 -0400

    pokemon rendering

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -4,12 +4,25 @@ class Pokemon extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.getPokemonImage=this.getPokemonImage.bind(this)
   }
 
+  getPokemonImage (obj) {
+    let src = obj.front_default
+    if(obj.front_default === null) src = obj.front_shiny
+    if(src !== null) return src
+    if(obj.front_shiny === null) src = obj.front_female 
+    if(src !== null) return src
+    if(obj.front_shiny_female === null) return src
+  }
   render() {
     return (
       <div>
-        {this.props.pokemon.name}
+        You are a {this.props.pokemon.name}!
+        <br></br><br></br><br></br><br></br>
+
+          <img src={this.getPokemonImage(this.props.pokemon.sprites)} />
+      
         <br></br><br></br><br></br><br></br>
         <button
           onClick={e => {
