2e5959ca0802134b81197a2b1920520b36042449
seo218 <casey.seo1@gmail.com>
2019-08-30 11:21:05 -0400

    page 5 upated

--- src/components/page5.js
+++ src/components/page5.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Form, Button, Badge, Dropdown } from "react-bootstrap";
+import { Card, Button, Dropdown } from "react-bootstrap";
+import image from '../../database/redblueyellow.jpg'
 
 class Page5 extends React.Component {
   constructor(props) {
@@ -17,73 +18,74 @@ class Page5 extends React.Component {
   }
   render() {
     return (
-      <Form>
-        <h1>
-          <br />
-          <Badge variant="danger">
-            I would trade my Blue and Red Version for Yellow Version
-          </Badge>
-          <br />
-          <br />
-        </h1>
-        <h2 />
-        <Dropdown>
-          <Dropdown.Toggle variant="success" id="dropdown-basic">
-            {this.state.dropdownMenu}
-          </Dropdown.Toggle>
-          <Dropdown.Menu>
-            <Dropdown.Item
-              href="#/action-1"
-              onClick={() => {
-                this.handleDropdownMenuChange("Strongly agree");
-              }}
-            >
-              Strongly agree
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-2"
-              onClick={() => {
-                this.handleDropdownMenuChange("Agree");
-              }}
-            >
-              Agree
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-3"
-              onClick={() => {
-                this.handleDropdownMenuChange("Neither agree or disagree");
-              }}
-            >
-              Neither agree or disagree
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-4"
-              onClick={() => {
-                this.handleDropdownMenuChange("Disagree");
-              }}
-            >
-              Disagree
-            </Dropdown.Item>
-            <Dropdown.Item
-              href="#/action-5"
-              onClick={() => {
-                this.handleDropdownMenuChange("Strongly disagree");
-              }}
-            >
-              Strongly disagree
-            </Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-        <br />
-        <br />
-        <Button
-          onClick={e => {
-            this.props.formComplete(e);
-          }}
-        >
-          next
-        </Button>
-      </Form>
+      <div className="fourthPageCards">
+      <Card border="danger" bg="dark" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>I would...</Card.Title>
+          <Card.Text>
+            Trade my blue and red version for a yellow version
+</Card.Text>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              {this.state.dropdownMenu}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                href="#/action-1"
+                onClick={() => {
+                  this.handleDropdownMenuChange("Strongly agree");
+                }}
+              >
+                Strongly agree
+</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-2"
+                onClick={() => {
+                  this.handleDropdownMenuChange("Agree");
+                }}
+              >
+                Agree
+</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-3"
+                onClick={() => {
+                  this.handleDropdownMenuChange("Neither agree or disagree");
+                }}
+              >
+                Neither agree or disagree
+</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-4"
+                onClick={() => {
+                  this.handleDropdownMenuChange("Disagree");
+                }}
+              >
+                Disagree
+</Dropdown.Item>
+              <Dropdown.Item
+                href="#/action-5"
+                onClick={() => {
+                  this.handleDropdownMenuChange("Strongly disagree");
+                }}
+              >
+                Strongly disagree
+</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Card.Body>
+      </Card>
+      <br />
+      <br />
+    <br></br>
+    <Button
+      onClick={e => {
+        this.props.formComplete(e);
+      }}
+    >
+      next
+</Button>
+  </div>
     );
   }
 }
